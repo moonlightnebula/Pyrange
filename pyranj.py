e8d0d8a01ebe4353ef4eff55372ff73a40db3f68
mahtab shafiei <mahtab.shafiei@outlook.com>
2023-07-28 14:31:15 +0330

    discard error

--- pyranj.py
+++ pyranj.py
@@ -1,14 +1,19 @@
-import logging
+# from logging import Logger
 
 class PyRanj () :
-    
+    def __init__(self , *args , **kwargs):
+        print('[EXCEPTION] :: Ranj')
     def wrapper(self , *args , **kwargs):
-        # logging.Logger('[EXCEPTION]' , )
-        logging.log([Exception], *args)
+        print('[EXCEPTION] :: Ranj')
+
 
 
 if __name__=="__main__":
-    @PyRanj
+    from pyranj import PyRanj as pyranj
+
+    @pyranj
     def f():
         raise Exception('Ranj')
+
+    
 
